661663d77306686feeca2cbfacbd00c7672f3bd3
Jake Ketcheson <snowcave@gmail.com>
2014-08-19 10:15:18 -0700

    added conditionals to the html element for ie

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,8 @@
 <!doctype html>
-<html class="no-js" <?php language_attributes(); ?>>
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
